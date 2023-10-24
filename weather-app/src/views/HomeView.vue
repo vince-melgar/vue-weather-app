f7a078d3390d546ea6cb9c9774ea915db65c7c76
vince-melgar <118233473+vince-melgar@users.noreply.github.com>
2023-10-25 05:47:07 +0800

    Skeleton LOading Added

--- weather-app/src/views/HomeView.vue
+++ weather-app/src/views/HomeView.vue
@@ -37,7 +37,7 @@
       <Suspense>
         <CityList />
         <template #fallback>
-          <p>Loading...</p>
+          <CityCardSkeleton />
         </template>
       </Suspense>
     </div>
@@ -49,6 +49,7 @@ import { ref } from "vue";
 import axios from "axios";
 import { useRouter } from "vue-router";
 import CityList from "../components/CityList.vue";
+import CityCardSkeleton from "../components/CityCardSkeleton.vue";
 
 const router = useRouter();
 const previewCity = (searchResult) => {
